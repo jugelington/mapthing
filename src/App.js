f19bbcbdf8a8be29293cbb9ffe048064416156a3
jugelington <julianbeightondykes@gmail.com>
2018-11-07 15:45:28 +0000

    second tile layer

--- src/App.js
+++ src/App.js
@@ -9,15 +9,24 @@ class App extends Component {
   }
 
   componentDidMount() {
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    var mymap = L.map('mapid').setView([51.505, -0.09], 1);
+
     L.tileLayer(
-      'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoianVsaWFuYmVpZ2h0b25keWtlcyIsImEiOiJjam83OWswZmYwYmJpM3FwN3M2NHFld2x1In0.PDZj9MvL5YOKm_wE5PIB3A',
+      'https://api.tiles.mapbox.com/v4/{id}/1/0/0.png?access_token=pk.eyJ1IjoianVsaWFuYmVpZ2h0b25keWtlcyIsImEiOiJjam83OWswZmYwYmJpM3FwN3M2NHFld2x1In0.PDZj9MvL5YOKm_wE5PIB3A',
       {
         attribution:
           'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         id: 'mapbox.streets',
         accessToken: 'your.mapbox.access.token'
+      }
+    ).addTo(mymap);
+
+    L.tileLayer(
+      'https://tile.openweathermap.org/map/clouds_new/1/0/0.png?appid=ae4631f207f116f90af983c8651c3948',
+      {
+        maxZoom: 18,
+        id: 'clouds'
       }
     ).addTo(mymap);
   }
